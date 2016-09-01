8d156606693649059107ba4b1e03e2fb60669217
Tom <tjpalmer@tjpalmer.com>
2016-09-01 05:53:41 -0700

    Replace vars and lambdas

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Collections.ObjectModel;
 
 namespace World {
 
@@ -36,10 +38,92 @@ public class Program {
 
 }
 
+class MyParameter: Expression {
+
+  public MyParameter(Type type, string name) {
+    this.type = type;
+    this.Name = name;
+  }
+
+  public sealed override ExpressionType NodeType {
+    get {return ExpressionType.Parameter;}
+  }
+
+  public readonly string Name;
+
+  public override string ToString() {
+    // TODO Change this back to just the name.
+    // TODO It's only extra so I can see that I've got it in place.
+    return $"{Name}!?!";
+  }
+
+  public sealed override Type Type {
+    get {return type;}
+  }
+
+  private Type type;
+
+}
+
+class MyLambda: Expression {
+
+  public MyLambda(
+    Type type,
+    Expression body,
+    ReadOnlyCollection<Expression> parameters
+  ) {
+    this.Body = body;
+    this.Parameters = parameters;
+    this.type = type;
+  }
+
+  public readonly Expression Body;
+
+  public sealed override ExpressionType NodeType {
+    get {return ExpressionType.Lambda;}
+  }
+
+  public readonly ReadOnlyCollection<Expression> Parameters;
+
+  public override string ToString() {
+    return string.Join(" ",
+      Parameters.Count == 1 ?
+        Parameters[0].ToString() :
+        $"({string.Join(", ", Parameters)})",
+      "=>",
+      Body
+    );
+  }
+
+  public sealed override Type Type {
+    get {return type;}
+  }
+
+  private readonly Type type;
+
+}
+
 class Visitor: ExpressionVisitor {
   protected override Expression VisitConstant(ConstantExpression node) {
-    Console.WriteLine($"Found constant: {node.Type} {node.Value}");
+    Console.WriteLine($"Found constant: {node.Type} {node.NodeType} {node.Value}");
     return Expression.Constant(3.0);
+  }
+
+  protected override Expression VisitLambda<T>(Expression<T> node) {
+    return new MyLambda(
+      // The same type should apply if we just replace old parameters with our
+      // metadata-carrying equivalents.
+      node.Type,
+      Visit(node.Body),
+      new List<Expression>(
+        from parameter in node.Parameters select Visit(parameter)
+      ).AsReadOnly()
+    );
+  }
+
+  protected override Expression VisitParameter(ParameterExpression node) {
+    Console.WriteLine($"Found parameter: {node.Type} {node.NodeType} {node.Name}");
+    return new MyParameter(node.Type, node.Name);
   }
 }
 
